976f575f9aba75c929a404fad56b5636db0b0e93
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2021-01-16 14:19:10 +0600

    Create list4.cpp

---
+++ list4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+
+using namespace std;
+
+
+int main()
+{
+	int arr[5] = {5,4,3,2,1};
+	
+	//copy array to list
+	
+	list<int>mylist(arr,arr+5);
+	
+	//reverse this list
+	mylist.reverse();	
+	
+	list<int>::iterator it;
+	
+	for(it = mylist.begin(); it!=mylist.end(); it++)
+	{
+		cout<<*it<<" ";
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
